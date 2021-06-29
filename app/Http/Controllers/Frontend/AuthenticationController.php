b0d0297e10b5ad8553519e2d7adf4c8aba56bc74
sagun gurung <sagungurung614@gmail.comclear>
2021-06-29 20:58:59 +0545

    seller table init

--- app/Http/Controllers/Frontend/AuthenticationController.php
+++ app/Http/Controllers/Frontend/AuthenticationController.php
@@ -4,19 +4,22 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Frontend\Visitor;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AuthenticationController extends Controller
 {
     public function register(){
-        dd($token = rand(1,25););
+        // dd($expiry_token = Carbon::now());
         $user = Visitor::all();
         return view('frontend.authenticate.register');
     }
     public function submitRegister(Request $request){
         // dd($request); //do or die
         // dd($request);
+        DB::beginTransaction();
         $validated = $request->validate([
             'name'=>'required|min:3|max:50',
             'email'=>'required|email|unique:visitors',//same email cannot be used by different user
@@ -31,8 +34,12 @@ class AuthenticationController extends Controller
         $visitor->password = $password;
         $saved = $visitor->save();
         if($saved){
-            $token = rand(25);
+            $visitor = Visitor::where('id',$visitor->id)->first();
+            $visitor->verify_token = \Str::random(80);
+            $visitor->token_expiry = Carbon::now();
+            $visitor->update();
         }
+        DB::commit();
         return redirect()->back()->with(['success'=>'Registered Successfully.']);   
     }
 
--- database/migrations/2021_06_29_130719_create_sellers_table.php
+++ database/migrations/2021_06_29_130719_create_sellers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateVisitorsTable extends Migration
+class CreateSellersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,17 @@ class CreateVisitorsTable extends Migration
      */
     public function up()
     {
-        Schema::create('visitors', function (Blueprint $table) {
+        Schema::create('sellers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('phone no');
+            $table->text('address')->nullable();
+            $table->string('shopname')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('verify_token')->nullable();
-            $table->dateTime('token_expiry')->nullable();
-            $table->integer('status')->default(1)->comment('0-suspended,1-suspended,2-unverified');
-            $table->rememberToken();
+            $table->text('image')->nullable();
+            $table->text('bio');
             $table->timestamps();
         });
     }
@@ -34,6 +35,6 @@ class CreateVisitorsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('visitors');
+        Schema::dropIfExists('sellers');
     }
 }
